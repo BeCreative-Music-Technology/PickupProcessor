52abbd62e57bd725b6fc490999e28b5f6f0e4970
TijnVanBekhoven <113771373+TijnVanBekhoven@users.noreply.github.com>
2026-04-21 15:32:53 +0200

    cpal-test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+mod stream;
+
+use cpal::traits::StreamTrait;
+use ringbuf::HeapRb;
+use ringbuf::traits::Split;
+use crate::stream::{InputStream, OutputStream};
+
+fn main() -> Result<(), anyhow::Error> {
+    let ring_buffer = HeapRb::<f32>::new(2048);
+    let (producer, consumer) = ring_buffer.split();
+
+    let input_stream = InputStream::open(producer);
+    let output_stream = OutputStream::open(consumer);
+
+    input_stream.stream.play()?;
+    output_stream.stream.play()?;
+
+    loop {}
 }
