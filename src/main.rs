778f2645d38dd2046a17f6e5161231b1728d2cad
TijnVanBekhoven <113771373+TijnVanBekhoven@users.noreply.github.com>
2026-05-06 09:58:36 +0200

    Reset the project

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,3 @@
-mod stream;
-
-use cpal::traits::StreamTrait;
-use ringbuf::HeapRb;
-use ringbuf::traits::Split;
-use crate::stream::{InputStream, OutputStream};
-
-fn main() -> Result<(), anyhow::Error> {
-    let ring_buffer = HeapRb::<f32>::new(2048);
-    let (producer, consumer) = ring_buffer.split();
-
-    let input_stream = InputStream::open(producer);
-    let output_stream = OutputStream::open(consumer);
-
-    input_stream.stream.play()?;
-    output_stream.stream.play()?;
-
-    loop {}
+fn main() {
+    println!("Hello, world!");
 }
