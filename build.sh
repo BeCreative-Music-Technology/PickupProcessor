9602674686dcce896a5ae004987d43ad92ad0c77
TijnVanBekhoven <113771373+TijnVanBekhoven@users.noreply.github.com>
2026-06-17 14:21:38 +0200

    Created build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+﻿# Pull latest changes from main
+git checkout main
+git pull
+echo "Fetched latest changes from repository"
+
+# Clean project and build as release
+cargo clean
+cargo build --release
+echo "Built project"
