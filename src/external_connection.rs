288aedad66348b3c99400c43fd35521d304a9396
TijnVanBekhoven <113771373+TijnVanBekhoven@users.noreply.github.com>
2026-07-02 12:27:34 +0200

    Updated the external_connection.rs

--- src/external_connection.rs
+++ src/external_connection.rs
@@ -56,50 +56,48 @@ impl VcsgpConnection {
       let mut effect: Box<dyn AudioEffect> = match effect_dto.effect_type {
         EffectType::Gain => {
           let gain = match Self::get_parameter("gain", effect_dto.parameters) {
-            Ok(gain) => gain,
-            Err(e) => { logger::error(LOG_ENVIRONMENT, e); return },
+            Some(gain) => gain,
+            None() => { logger::error_str(LOG_ENVIRONMENT, "gain parameter not found"); return },
           };
           Box::new(GainEffect::new(effect_dto.mix, gain))
         }
         EffectType::LowPassFilter => {
           let frequency = match Self::get_parameter("frequency", effect_dto.parameters) {
-            Ok(frequency) => frequency,
-            Err(e) => { logger::error(LOG_ENVIRONMENT, e); return },
+            Some(frequency) => frequency,
+            None() => { logger::error_str(LOG_ENVIRONMENT, "frequency parameter not found"); return },
           };
           let q_factor = match Self::get_parameter("q_factor", effect_dto.parameters) {
-            Ok(q_factor) => q_factor,
-            Err(e) => { logger::error(LOG_ENVIRONMENT, e); return },
+            Some(q_factor) => q_factor,
+            None() => { logger::error_str(LOG_ENVIRONMENT, "q_factor parameter not found"); return },
           };
-          Box::new(LowPassFilterEffect::new(effect_dto.mix, frequency, q_factor)) 
+          Box::new(LowPassFilterEffect::new(effect_dto.mix, frequency, q_factor))
         },
         EffectType::Reverb => {
           let room_size = match Self::get_parameter("room_size", effect_dto.parameters) {
-            Ok(room_size) => room_size,
-            Err(e) => { logger::error(LOG_ENVIRONMENT, e); return },
+            Some(room_size) => room_size,
+            None() => { logger::error_str(LOG_ENVIRONMENT, "room_size parameter not found"); return },
           };
-          let reverb_decay = match Self::get_parameter("reverb_decay", effect_dto.parameters) {
-            Ok(reverb_decay) => reverb_decay,
-            Err(e) => { logger::error(LOG_ENVIRONMENT, e); return },
+          let reverb_decay = match Self::get_parameter("decay", effect_dto.parameters) {
+            Some(reverb_decay) => reverb_decay,
+            None() => { logger::error_str(LOG_ENVIRONMENT, "decay parameter not found"); return },
           };
           let dampening = match Self::get_parameter("dampening", effect_dto.parameters) {
-            Ok(dampening) => dampening,
-            Err(e) => { logger::error(LOG_ENVIRONMENT, e); return },
+            Some(dampening) => dampening,
+            None() => { logger::error_str(LOG_ENVIRONMENT, "dampening parameter not found"); return },
           };
           Box::new(ReverbEffect::new(effect_dto.mix, room_size, reverb_decay, dampening))
         },
         EffectType::Delay => {
           let delay = match Self::get_parameter("delay", effect_dto.parameters) {
-            Ok(delay) => delay,
-            Err(e) => { logger::error(LOG_ENVIRONMENT, e); return },
+            Some(delay) => delay,
+            None() => { logger::error_str(LOG_ENVIRONMENT, "delay parameter not found"); return },
           };
-          Box::new(DelayEffect::new(effect_dto.mix, delay)) 
+          Box::new(DelayEffect::new(effect_dto.mix, delay))
         },
       };
 
       // Set effect parameters and attach control inputs
       effect_dto.parameters.iter().for_each(|parameter_dto| {
-        _ = effect.set_value(parameter_dto.key.as_str(), parameter_dto.value);
-
         let observer = match effect.get_control_observer(parameter_dto.key.as_str()) {
           Ok(observer) => observer,
           Err(e) => {
@@ -124,8 +122,10 @@ impl VcsgpConnection {
     });
   }
 
-  fn get_parameter(key: &str, parameter_list: Vec<EffectParameterDto>) -> Result<u16, Error> {
-
+  fn get_parameter(key: &str, parameters: Vec<EffectParameterDto>) -> Option<u16> {
+    parameters.iter()
+      .find(|p| p.key == key)
+      .map(|p| p.value)
   }
 }
 
